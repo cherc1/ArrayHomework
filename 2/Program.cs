451c271031071c1b14ae0b8a9603c8ea9bf71a9f
Arina Skripova <skripovaarina1@gmail.com>
2023-12-02 16:23:27 +0300

    №2

---
+++ 2/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] array = new int[10] {1,2,3,4,5,6,7,8,9,10};
+int kol = 0;
+for(int i = 0;i < array.Length; i++)
+if(array[i] % 2 == 0)
+{
+    kol++;
+}
+Console.WriteLine("Ответ = " + kol);
