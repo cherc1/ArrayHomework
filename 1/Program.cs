395641386e188b785eca7529213d125f4c0586f5
Arina Skripova <skripovaarina1@gmail.com>
2023-12-02 16:07:14 +0300

    №1

---
+++ 1/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = new int [11];
+for (int i = 0; i < array.Length; i++)
+array[i] = new Random().Next(1, 100);  
+Console.WriteLine(string.Join(", ", array));
+int n = 0; int kol = 0;
+while(n <= array.Length)
+{
+    if(array[n] > 20 && array[n] < 90)
+    {
+        kol++;
+    }
+    n++;
+}
+Console.WriteLine("Количество искомых = " + kol);
