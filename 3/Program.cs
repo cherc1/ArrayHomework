725654460079460b623de5275382805c71ec0210
Arina Skripova <skripovaarina1@gmail.com>
2023-12-02 17:57:23 +0300

    №3

---
+++ 3/Program.cs
@@ -0,0 +1,40 @@
+﻿System.Console.WriteLine("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [] array = new int [n];
+for (int k = 0; k < array.Length; k++)
+array[k] = new Random().Next(1, 10);  
+int min = 1;
+int max = 1;
+while(n <= array.Length)
+{
+    if(array[n] > max)
+    {
+        max = array[n];
+    }
+    n++;
+}
+while(n <= array.Length)
+{
+   if(array[n] < min)
+    {
+        min = array[n];
+    }
+    n++;
+}
+/*for (int i = 0; i <=array.Length;i++)
+{
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+}
+for (int i = 0; i <=array.Length;i++)
+{
+    if(array[i] < min)
+    {
+        min = array[i];
+    }
+}*/
+Console.WriteLine("Минимальное число - " + min + ", максимальное число - " + max);
+int iskomoe = max - min;
+Console.WriteLine("Ответ: " + iskomoe);
